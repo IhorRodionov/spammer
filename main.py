6ea258927b37cd730a73a2567884f0a058b95c09
IhorRodionov <73175941+IhorRodionov@users.noreply.github.com>
2022-04-25 12:52:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from telethon import TelegramClient
+import asyncio
+import os
+import mysql.connector as con
+
+server = '185.233.38.219'
+database = 'bulbulator'
+username = 'spadmin'
+password = 'admin123'
+connection = con.connect(host=server,
+                         database=database,
+                         user=username,
+                         password=password)
+
+cursor = connection.cursor()
+cursor.execute("select name from attacklist;")
+spam_list = list(map(lambda x: str(x[0]), cursor.fetchall()))
+cursor.close()
+
+fds = os.listdir("photo")
+list_name = list(map(lambda x: "photo/"+x, fds))
+
+
+async def spam(client:TelegramClient):
+    for bot in spam_list:
+        await client.send_message(bot, "/start")
+        for img in list_name:
+            await client.send_file(bot, img)
+
+async def user(name, api_id, api_hash):
+    async with TelegramClient(name, api_id, api_hash) as client:
+        await spam(client)
+
+
+async def main():
+    while True:
+        cursor = connection.cursor()
+        cursor.execute("select name, api_id, api_hash from people;")
+        tasks=[]
+        rows = cursor.fetchall()
+        for row in rows:
+            tasks.append(asyncio.get_event_loop().create_task(user(str(row[0]),
+                                                               int(row[1]),
+                                                               str(row[2])
+                                                               )))
+        await asyncio.wait(tasks)
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
+
+
